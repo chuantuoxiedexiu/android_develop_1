452033490fdf65c5e5b84abe68aa9c460554fa10
zxwang <314997217@qq.com>
2018-02-23 16:17:38 +0100

    three

--- app/src/main/java/com/example/xuan/helloworld/MainActivity.java
+++ app/src/main/java/com/example/xuan/helloworld/MainActivity.java
@@ -1,80 +1,33 @@
 package com.example.xuan.helloworld;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.app.Activity;
 import android.view.Menu;
-import android.view.MenuItem;
+import android.content.Intent;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
-    public static final String TAG ="MyMessage";
+public class MainActivity extends Activity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        // Print Log
-        Log.i(TAG,"onCreate");
     }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        // Print Log
-        Log.i(TAG,"onStop");
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        // Print Log
-        Log.i(TAG,"onDestroy");
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        // Print Log
-        Log.i(TAG,"onPause");
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        // Print Log
-        Log.i(TAG,"onResume");      }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-        // Print Log
-        Log.i(TAG,"onStart");
-    }
-
-    @Override
-    protected void onRestart() {
-        super.onRestart();
-        // Print Log
-        Log.i(TAG,"onRestart");      }
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        // Print Log
-        Log.i(TAG, "onCreateOptionsMenu");
+        getMenuInflater().inflate(R.menu.activity_main, menu);
         return true;
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Print Log
-        Log.i(TAG, "onOptionsItemSelected");
-
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        return super.onOptionsItemSelected(item);
+    // Method to start the service
+    public void startService(View view) {
+        startService(new Intent(getBaseContext(), MyService.class));
     }
+
+    // Method to stop the service
+    public void stopService(View view) {
+        stopService(new Intent(getBaseContext(), MyService.class));
+    }
+
+
 }
